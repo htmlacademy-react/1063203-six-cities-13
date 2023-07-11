136de357566427d8538e9172e80cb843afbc4b35
Konstantin Pavlyuts <makemewanna@yandex.ru>
2023-07-11 20:24:56 +0300

    добавляем приватные роуты и список всех роутов на главную страницу, module3-task1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,14 @@
-import mainScreen from '../../pages/main-screen/main-screen.tsx';
+import MainScreen from '../../pages/main-screen/main-screen.tsx';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { AppRoute } from '../../routes';
+import LoginScreen from '../../pages/login-screen/login-screen.tsx';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen.tsx';
+import OfferScreen from '../../pages/offer-screen/offer-screen.tsx';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen.tsx';
+import PrivateRoute from '../private-route/private-route.tsx';
+import {AuthStatus} from '../../const/authStatus.ts';
 
 type AppProps = {
   cardsCount: number;
@@ -7,7 +16,42 @@ type AppProps = {
 
 function App({cardsCount}: AppProps): React.ReactElement {
   return (
-    mainScreen({ cardsCount })
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Home}
+            element={<MainScreen cardsCount={cardsCount} />}
+          />
+
+          <Route
+            path={AppRoute.Login}
+            element={<LoginScreen />}
+          />
+
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthStatus.NoAuth}
+              >
+                <FavoritesScreen />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path={AppRoute.Offer}
+            element={<OfferScreen />}
+          />
+
+          <Route
+            path="*"
+            element={<NotFoundScreen />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
---
+++ src/components/private-route/private-route.tsx
@@ -0,0 +1,21 @@
+import { Navigate } from 'react-router-dom';
+import { AppRoute } from '../../routes';
+import React from 'react';
+import { AuthStatus } from '../../const/authStatus.ts';
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthStatus;
+  children: React.ReactElement;
+}
+
+function PrivateRoute(props: PrivateRouteProps): React.ReactElement {
+  const {authorizationStatus, children} = props;
+
+  return (
+    authorizationStatus === AuthStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.Login} />
+  );
+}
+
+export default PrivateRoute;
