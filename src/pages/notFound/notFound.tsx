b6d5365933ab2b5d088d7b6ed2a950dc885b7c12
Konstantin Pavlyuts <makemewanna@yandex.ru>
2023-07-15 08:14:14 +0300

    useEffect with navigation, module4-task1

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function NotFound(): React.ReactElement{
   return (
-    <>
+    <div className="container">
       <h1>404. Page not found</h1>
       <Link to="/">Back to main page</Link>
-    </>
+    </div>
   );
 }
 
